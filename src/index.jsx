d498e1e84c54fc8cf9bcdb1096e66e03598bb54d
TheKhaliqAnsari <khaliq.ansari@sarvagram.com>
2025-08-26 23:51:06 +0530

    vercel analytics added

--- src/index.jsx
+++ src/index.jsx
@@ -3,12 +3,14 @@ import ReactDOM from "react-dom/client";
 import "./index.css";
 import App from "./App";
 import { BrowserRouter as Router } from "react-router-dom";
+import { Analytics } from "@vercel/analytics/react";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
     <Router basename="/">
       <App />
+      <Analytics />
     </Router>
   </React.StrictMode>
 );
